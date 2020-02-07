39e20e34585022b224fed25b7eb0e5122a6ccf4a
ihsan2 <nihsan727@gmail.com>
2020-02-07 09:52:50 +0700

    Update README.md

--- src/components/ListSaham.js
+++ src/components/ListSaham.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text, ScrollView} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
