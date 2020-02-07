a4ff70677cd94cd99b29943284e657800de0c305
ihsan2 <nihsan727@gmail.com>
2020-02-07 10:07:04 +0700

    Fix App.js Error

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Screen1 from './src/Screen1';
-import Screen2 from './src/Screen2';
 
 const Stack = createStackNavigator();
 
@@ -14,7 +13,6 @@ function App() {
           headerShown: false,
         }}>
         <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
